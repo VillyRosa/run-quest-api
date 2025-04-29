625b2bdcbbd96db4801c9789bc88e9dd24608d4e
VillyRosa <villyrosa@hotmail.com>
2025-04-29 17:41:25 -0300

    feat(users): create delete my account method

--- src/main/java/com/runquest/api/controller/UserController.java
+++ src/main/java/com/runquest/api/controller/UserController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -27,5 +24,11 @@ public class UserController {
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDTO> getById(@PathVariable UUID id) {
         return ResponseEntity.ok().body(userService.findById(id));
+    }
+
+    @DeleteMapping("/me")
+    public ResponseEntity<Void> deleteMyAccount() {
+        userService.deleteMyAccount();
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/runquest/api/domain/user/UserService.java
+++ src/main/java/com/runquest/api/domain/user/UserService.java
@@ -1,5 +1,6 @@
 package com.runquest.api.domain.user;
 
+import com.runquest.api.domain.auth.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,11 +13,19 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private AuthService authService;
+
     public Page<UserResponseDTO> findAll(Pageable pageable) {
         return userRepository.findAll(pageable).map(UserResponseDTO::new);
     }
 
     public UserResponseDTO findById(UUID id) {
         return userRepository.findById(id).map(UserResponseDTO::new).orElse(null);
+    }
+
+    public void deleteMyAccount() {
+        UUID id = authService.getAuthenticatedUserId();
+        userRepository.deleteById(id);
     }
 }
